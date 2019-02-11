eb63989225020f815c0ff3652105334cee3ad0f5
JY <snowink1137@gmail.com>
2019-02-11 17:59:24 +0900

    20190211 end of ssafy | django local prj

---
+++ 1.first-semester/07_django/base_prj/sns/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Posting, Comment
+
+
+def posting_list(request):
+    postings = Posting.objects.all()
+    context = {
+        'postings': postings
+    }
+    return render(request, 'sns/list.html', context)
+
+
+def posting_detail(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    comments = posting.comment_set.all()
+    context = {
+        'posting': posting,
+        'comments': comments,
+    }
+
+    return render(request, 'sns/detail.html', context)
+
+
+def create_posting(request):
+    if request.method == 'POST':
+        posting = Posting()
+        posting.content = request.POST.get('content')
+        posting.icon = request.POST.get('icon')
+        posting.image = request.FILES.get('image')
+        posting.save()
+        return redirect('sns:posting_detail', posting.id)
+
+    else:
+        return redirect('sns:posting_list')
+
+
+def edit_posting(request):
+    pass
+
+
+def delete_posting(request):
+    pass
+
+
+def create_comment(request, posting_id):
+    posting = get_object_or_404(Posting, id=posting_id)
+    if request.method == 'POST':
+        comment = Comment()
+        comment.content = request.POST.get('comment')
+        comment.posting = posting
+        comment.save()
+
+        return redirect('sns:posting_detail', posting.id)
