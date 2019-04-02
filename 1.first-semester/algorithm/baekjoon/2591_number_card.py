6630aa355facf8ed2cf2b268ce5ad45df5992cbe
snowink1137 <snowink1137@gmail.com>
2019-04-03 01:45:44 +0900

    self study

--- 1.first-semester/algorithm/baekjoon/2591_number_card.py
+++ 1.first-semester/algorithm/baekjoon/2591_number_card.py
@@ -3,32 +3,19 @@ import sys
 sys.stdin = open('2591.txt', 'r')
 
 numbers = list(map(int, list(input())))
+numbers.insert(0, 0)
 N = len(numbers)
-memo = [0] * (N+1)
+memo = [0] * 41
 memo[1] = 1
-flag = False
-if N > 1:
-    memo[2] = 2
-    for i in range(3, N+1):
-        memo[i] = memo[i-1] + memo[i-2]
+check = N
 
-    result = 1
-    cnt = 1
-    for i in range(N-1):
-        if numbers[i] == 0 or numbers[i+1] == 0:
-            flag = True
-            break
+for i in range(2, N):
+    if numbers[i] > 0:
+        memo[i] += memo[i-1]
 
-        if numbers[i]*10 + numbers[i+1] <= 34:
-            cnt += 1
-        else:
-            result *= memo[cnt]
-            cnt = 1
+    if 10 <= 10 * numbers[i-1] + numbers[i] <= 34:
+        memo[i] += memo[i-2]
+        if numbers[i-1] == 0:
+            check -= 1
 
-    if flag:
-        print(0)
-    else:
-        result *= memo[cnt]
-        print(result)
-else:
-    print(1)
+print(memo[check])
