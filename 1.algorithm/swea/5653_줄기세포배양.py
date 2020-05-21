f9c6a44cc14027d2ed1d48fca034188037a629c6
unknown <snowink1137@gmail.com>
2020-05-21 23:33:41 +0900

    [swea] 줄기세포배양

---
+++ 1.algorithm/swea/5653_줄기세포배양.py
@@ -0,0 +1,60 @@
+import sys
+
+sys.stdin = open('5653_sample_input.txt', 'r')
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+T = int(input())
+for test_case in range(1, T+1):
+    N, M, K = map(int, input().split())
+    result = 0
+
+    matrix = [[0 for _ in range(350)] for _ in range(350)]
+
+    for i in range(N):
+        row = list(map(int, input().split()))
+        for j in range(M):
+            temp = row.pop(0)
+
+            if temp != 0:
+                matrix[i+150][j+150] = [temp, temp]
+
+    for _ in range(1, K+1):
+        queue = []
+
+        for i in range(350):
+            for j in range(350):
+                if matrix[i][j] == 0:
+                    continue
+
+                if matrix[i][j][1] > 0:
+                    matrix[i][j][1] -= 1
+                elif matrix[i][j][1] == 0:
+                    matrix[i][j][1] -= 1
+                    queue.append((i, j))
+                elif matrix[i][j][0] == abs(matrix[i][j][1]):
+                    pass
+                else:
+                    matrix[i][j][1] -= 1
+                    queue.append((i, j))
+
+        while len(queue):
+            q = queue.pop()
+
+            for i in range(4):
+                new_x = q[0] + dx[i]
+                new_y = q[1] + dy[i]
+
+                if matrix[new_x][new_y] != 0:
+                    if matrix[new_x][new_y][0] == matrix[new_x][new_y][1] and matrix[q[0]][q[1]][0] > matrix[new_x][new_y][0]:
+                        matrix[new_x][new_y] = [matrix[q[0]][q[1]][0], matrix[q[0]][q[1]][0]]
+                else:
+                    matrix[new_x][new_y] = [matrix[q[0]][q[1]][0], matrix[q[0]][q[1]][0]]
+
+    for i in range(350):
+        for j in range(350):
+            if matrix[i][j] != 0 and (matrix[i][j][0] > abs(matrix[i][j][1]) or matrix[i][j][0] == matrix[i][j][1]):
+                result += 1
+
+    print('#{} {}'.format(test_case, result))
