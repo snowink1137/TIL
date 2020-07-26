b3854041f93d3b4fd0f3def2e60e5a38688b151b
JY <snowink1137@gmail.com>
2020-07-26 16:56:21 +0900

    [codeit] Java study - card shuffle

--- 4.Java/Exercise/Chapter03/CardShuffle/src/Card.java
+++ 4.Java/Exercise/Chapter03/CardShuffle/src/Card.java
@@ -1,2 +1,41 @@
 public class Card {
+    public final int suitNumber;
+    public final int rankNumber;
+
+    public Card(int suitNumber, int rankNumber) {
+        this.suitNumber = suitNumber;
+        this.rankNumber = rankNumber;
+    }
+
+    public String getSuit() {
+        if (suitNumber == 1) {
+            return "Clubs";
+        } else if (suitNumber == 2) {
+            return "Hearts";
+        } else if (suitNumber == 3) {
+            return "Diamonds";
+        } else if (suitNumber == 4) {
+            return "Spades";
+        } else {
+            return null;
+        }
+    }
+
+    public String getRank() {
+        if (rankNumber == 1) {
+            return "Ace";
+        } else if (rankNumber == 11) {
+            return "Jack";
+        } else if (rankNumber == 12) {
+            return "Queen";
+        } else if (rankNumber == 13) {
+            return "King";
+        } else {
+            return Integer.toString(rankNumber);
+        }
+    }
+
+    public String toString() {
+        return getRank() + " of " + getSuit();
+    }
 }
--- 4.Java/Exercise/Chapter03/CardShuffle/src/Deck.java
+++ 4.Java/Exercise/Chapter03/CardShuffle/src/Deck.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Deck {
+    private ArrayList<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<Card>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void print() {
+        for (Card card : cards) {
+            System.out.println(card.toString());
+        }
+    }
+
+    public void shuffle() {
+        Random random = new Random();
+        for (int i=0; i<cards.size(); i++) {
+            int temp = random.nextInt(cards.size());
+            Collections.swap(cards, i, temp);
+        }
+    }
+
+    public Deck deal(int count) {
+        Deck hand = new Deck();
+        if (count == 5) {
+            for (int i=0; i<5; i++) {
+                hand.addCard(cards.get(cards.size()-1));
+                cards.remove(cards.size()-1);
+            }
+        }
+        return hand;
+    }
 }
