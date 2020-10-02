8340c417426893790bc42cde6926923373d909ac
JY <snowink1137@gmail.com>
2020-10-02 18:07:18 +0900

    algorithm practice

---
+++ 1.algorithm/programmers/H-Index.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    answer = 0
+    return answer
+
+
+citations = [3, 0, 6, 1, 5]
+print(solution(citations))
--- 1.algorithm/programmers/프린터.py
+++ 1.algorithm/programmers/프린터.py
@@ -3,9 +3,17 @@ from collections import deque
 
 def solution(priorities, location):
     answer = 0
-    priorities_queue = deque(priorities)
+    priorities_queue = deque([(i, v) for i, v in enumerate(priorities)])
 
+    while priorities_queue:
+        q = priorities_queue.popleft()
 
+        if priorities_queue and max(priorities_queue, key=lambda x: x[1])[1] > q[1]:
+            priorities_queue.append(q)
+        else:
+            answer += 1
+            if q[0] == location:
+                break
 
     return answer
 
