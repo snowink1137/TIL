be339052350b712929d9b95837c81ad03d798184
JY <snowink1137@gmail.com>
2020-10-05 20:32:18 +0900

    algorithm practice

--- 1.algorithm/programmers/조이스틱.py
+++ 1.algorithm/programmers/조이스틱.py
@@ -1,37 +1,33 @@
 def solution(name):
     answer = 0
+    comback_move = 0
     name_ord_list = list(map(lambda x: abs(ord(x)-ord('A')) if abs(ord(x)-ord('A')) < 13 else 26-abs(ord(x)-ord('A')), name))
+    zero_max_cnt = 0
+    zero_last_idx = 0
 
-    index = 0
-    while True:
-        right = 1
-        left = 1
-
-        if not sum(name_ord_list):
-            break
-
-        if name_ord_list[index]:
-            answer += name_ord_list[index]
-            name_ord_list[index] = 0
-
-        for i in range(1, len(name)):
-            if name_ord_list[(index+i)%len(name)]:
-                break
-            else:
-                right += 1
-
-        for i in range(1, len(name)):
-            if name_ord_list[index-i]:
-                break
-            else:
-                left += 1
-
-        if right > left:
-            answer += left
-            index -= left
+    zero_cnt = 0
+    for i, n in enumerate(name_ord_list):
+        if not n:
+            zero_cnt += 1
+            if zero_cnt > zero_max_cnt:
+                zero_max_cnt = zero_cnt
+                zero_last_idx = i
         else:
-            answer += right
-            index = (index + right) % len(name)
+            answer += n
+            zero_cnt = 0
+
+    zero_first_idx = zero_last_idx - zero_max_cnt + 1
+
+    if zero_first_idx == 0 or zero_first_idx == len(name_ord_list) - 1:
+        answer += (len(name_ord_list) - 1) - zero_max_cnt
+    else:
+        after_zero_last_idx_cnt = (len(name_ord_list) - 1) - zero_last_idx
+        if zero_first_idx <= after_zero_last_idx_cnt:
+            comeback_move = (zero_first_idx - 1) * 2 + after_zero_last_idx_cnt
+        else:
+            comeback_move = (zero_first_idx - 1) + after_zero_last_idx_cnt * 2
+
+        answer += min(comeback_move, len(name_ord_list)-1)
 
     return answer
 
