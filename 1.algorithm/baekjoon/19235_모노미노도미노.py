ccf05af33c7091ce86ba8a60cb400f180d42dc95
JY <snowink1137@gmail.com>
2020-10-24 21:54:06 +0900

    algorithm practice

--- 1.algorithm/baekjoon/19235_모노미노도미노.py
+++ 1.algorithm/baekjoon/19235_모노미노도미노.py
@@ -2,29 +2,48 @@ import sys
 
 sys.stdin = open('19235_sample_input.txt', 'r')
 
-N = int(input().split())
+N = int(input())
 result = 0
 green_board = [[0 for _ in range(4)] for _ in range(6)]
 blue_board = [[0 for _ in range(4)] for _ in range(6)]
 
 
-def drop_block(y, board):
-    x = 0
+def drop_block(y, board, x=-1):
     while True:
+        x += 1
         if x == 6:
             return 5
 
         if board[x][y]:
-            return x-1
-
-        x += 1
-
-    return x
+            return x - 1
 
 
 def down(board):
     visit = {}
-    for x in range()
+    for x in range(4, -1, -1):
+        for y in range(4):
+            if visit.get((x, y)) is None and board[x][y]:
+                if y < 3 and board[x][y] == board[x][y+1]:
+                    idx1 = drop_block(y, board, x)
+                    idx2 = drop_block(y+1, board, x)
+                    idx = min(idx1, idx2)
+
+                    if idx != x:
+                        board[idx][y] = board[x][y]
+                        board[idx][y+1] = board[x][y+1]
+                        board[x][y] = 0
+                        board[x][y+1] = 0
+
+                    visit[(x, y)] = 1
+                    visit[(x, y+1)] = 1
+                else:
+                    idx = drop_block(y, board, x)
+
+                    if idx != x:
+                        board[idx][y] = board[x][y]
+                        board[x][y] = 0
+
+                    visit[(x, y)] = 1
 
 
 def check_result(board):
@@ -49,7 +68,17 @@ def check_result(board):
 
 
 def check_boundary(board):
-    pass
+    cnt = 0
+    for x in range(2):
+        if sum(board[x]):
+            cnt += 1
+
+    for _ in range(cnt):
+        for y in range(4):
+            for x in range(5, 0, -1):
+                board[x][y] = board[x-1][y]
+
+            board[0][y] = 0
 
 
 for block_number in range(1, N+1):
