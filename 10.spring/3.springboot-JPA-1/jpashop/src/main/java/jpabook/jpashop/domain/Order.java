c367a762b0b6fdd34e96acd659b8b596d905cbfe
JY <snowink1137@gmail.com>
2021-02-08 23:39:13 +0900

    [inflearn] jpa study - develop entity class

--- 10.spring/3.springboot-JPA-1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 10.spring/3.springboot-JPA-1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,10 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 public class Order {
 
     @Id
@@ -18,5 +22,17 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;  // 주문상태 [ORDER, CANCEL]
 
 }
