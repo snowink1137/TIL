64f9a7b8640e65ae9dd2b5c63e83d39c43b39034
JJY <snowink1137@gmail.com>
2021-02-22 21:24:45 +0900

    [inflearn] jpa study - controller develop

--- 10.spring/3.springboot-JPA-1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 10.spring/3.springboot-JPA-1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -46,7 +49,6 @@ public class OrderItem {
     //==조회 로직==//
 
     /**
-     *
      * 주문상품 전체 가격 조회
      */
     public int getTotalPrice() {
