ac9898c8edc609fba3996628bcd946709ae0b708
unknown <snowink1137@gmail.com>
2021-03-27 23:36:55 +0900

    [inflearn] jpa study - select collection object with JPA(raw entity, DTO, DTO+fetch join

--- 10.spring/3.springboot-JPA/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 10.spring/3.springboot-JPA/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -104,7 +104,6 @@ public class OrderRepository {
     }
 
     public List<Order> findAllWithMemberDelivery() {
-
         return em.createQuery(
                 "select o from Order o" +
                         " join fetch o.member m" +
@@ -113,4 +112,13 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
