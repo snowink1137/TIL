d6f774985c86cb12ec8ebdc5920f0dfd84ad979d
jjy <snowink1137@gmail.com>
2021-04-19 23:22:44 +0900

    algorithm practice

--- 1.algorithm/programmers/광고_삽입.py
+++ 1.algorithm/programmers/광고_삽입.py
@@ -1,21 +1,49 @@
 def solution(play_time, adv_time, logs):
-    answer = ''
-
-    play_time_timestamp = 0
-    play_time_list = play_time.split(':')
-    for i in range(3):
-        play_time_timestamp += play_time_list[2-i] * (60 ** i)
-
-    play_time_user_check = [0 for _ in range(play_time_timestamp)]
+    play_time_timestamp = get_timestamp(play_time)
+    accumulated_user = [0 for _ in range(play_time_timestamp+1)]
 
     for log in logs:
-        temp_list = log.split('-')
+        time_list = log.split('-')
+        start = get_timestamp(time_list[0])
+        end = get_timestamp(time_list[1])
 
+        accumulated_user[start] += 1
+        accumulated_user[end] -= 1
+
+    for i in range(1, len(accumulated_user)):
+        accumulated_user[i] = accumulated_user[i] + accumulated_user[i-1]
+
+    for i in range(1, len(accumulated_user)):
+        accumulated_user[i] = accumulated_user[i] + accumulated_user[i-1]
+
+    adv_timestamp = get_timestamp(adv_time)
+
+    max_user = accumulated_user[adv_timestamp]
+    answer_timestamp = 0
+    for i in range(adv_timestamp+1, play_time_timestamp+1):
+        temp = accumulated_user[i] - accumulated_user[i-adv_timestamp]
+        if temp > max_user:
+            max_user = temp
+            answer_timestamp = i - adv_timestamp + 1
+
+    hour, answer_timestamp = divmod(answer_timestamp, 3600)
+    answer = str(hour).zfill(2)
+
+    minute, answer_timestamp = divmod(answer_timestamp, 60)
+    answer += ':' + str(minute).zfill(2) + ':' + str(answer_timestamp).zfill(2)
 
     return answer
 
 
-play_time = "02:03:55"
-adv_time = "00:14:15"
-logs = ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]
+def get_timestamp(time):
+    timestamp = 0
+    play_time_list = time.split(':')
+    for i in range(3):
+        timestamp += int(play_time_list[2 - i]) * (60 ** i)
+    return timestamp
+
+
+play_time = "99:59:59"
+adv_time = "25:00:00"
+logs = ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]
 print(solution(play_time, adv_time, logs))
