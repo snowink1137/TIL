9697babe3d1ced92fd3d702973cb15baf0817fba
snowink1137 <snowink1137@gmail.com>
2021-07-06 18:50:38 +0900

    [inflearn] jpa study - jpql 기본 문법, 프로젝션, 페이징

--- 10.spring/3.springboot-JPA/jpql/src/main/java/jpql/JpaMain.java
+++ 10.spring/3.springboot-JPA/jpql/src/main/java/jpql/JpaMain.java
@@ -18,9 +18,32 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
+
+            em.flush();
+            em.clear();
+
+//            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+//                    .getResultList();
+//
+//            MemberDTO memberDTO = result.get(0);
+//            System.out.println("memberDTO.username = " + memberDTO.getUsername());
+//            System.out.println("memberDTO.age = " + memberDTO.getAge());
+
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            System.out.println("result.size = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
