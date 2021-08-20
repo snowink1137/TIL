476bd93384f16c89fcdfea06ae5700c3ee6aeb94
snowink1137 <snowink1137@gmail.com>
2021-08-20 18:51:05 +0900

    [inflearn] spring cloud study - orders microservice 생성

--- 10.spring/5.spring-cloud/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ 10.spring/5.spring-cloud/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package com.example.userservice.security;
 
+import javax.servlet.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,8 +13,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests()
+            .antMatchers("/**")
+            .hasIpAddress("127.0.0.1")
+            .and()
+            .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
