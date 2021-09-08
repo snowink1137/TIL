1dfcf5d51224b9a702c26c4b5bcf43e9b74144ef
snowink1137 <snowink1137@gmail.com>
2021-09-08 19:54:25 +0900

    [inflearn] spring data jpa study - 공통 인터페이스 사용, 메소드 이름으로 쿼리 생성, @Query, 스프링 데이터 JPA 페이징과 정렬

--- 10.spring/6.spring-data-JPA/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ 10.spring/6.spring-data-JPA/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,5 +1,7 @@
 package study.datajpa.repository;
 
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -16,8 +18,49 @@ public class MemberJpaRepository {
     return member;
   }
 
+  public void delete(Member member) {
+    em.remove(member);
+  }
+
+  public List<Member> findAll() {
+    // JPQL
+    return em.createQuery("select m from Member m", Member.class)
+        .getResultList();
+  }
+
+  public Optional<Member> findById(Long id) {
+    Member member = em.find(Member.class, id);
+    return Optional.ofNullable(member);
+  }
+
+  public long count() {
+    return em.createQuery("select count(m) from Member m", Long.class)
+        .getSingleResult();
+  }
+
   public Member find(Long id) {
     return em.find(Member.class, id);
+  }
+
+  public List<Member> findByUsernameAndGreaterThan(String username, int age) {
+    return em.createQuery("select m from Member m where m.username = :username and m.age > :age")
+        .setParameter("username", username)
+        .setParameter("age", age)
+        .getResultList();
+  }
+
+  public List<Member> findByPage(int age, int offset, int limit) {
+    return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+        .setParameter("age", age)
+        .setFirstResult(offset)
+        .setMaxResults(limit)
+        .getResultList();
+  }
+
+  public long totalCount(int age) {
+    return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+        .setParameter("age", age)
+        .getSingleResult();
   }
 
 }
--- 10.spring/6.spring-data-JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ 10.spring/6.spring-data-JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,41 @@
 package study.datajpa.repository;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+  List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+  List<Member> findHelloBy();
+
+  List<Member> findTop3HelloBy();
+
+  @Query("select m from Member m where m.username = :username and m.age = :age")
+  List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+  @Query("select m.username from Member m")
+  List<String> findUsernameList();
+
+  @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+  List<MemberDto> findMemberDto();
+
+  @Query("select m from Member m where m.username in :names")
+  List<Member> findByNames(@Param("names") List<String> names);
+
+  List<Member> findListByUsername(String username);  // 컬렉션 반환 가능
+  Member findMemberByUsername(String username);  // 단건 반환 가능
+  Optional<Member> findOptionalByUsername(String username);  // 단건 Optional 반환 가능
+
+  @Query(value = "select m from Member m left join m.team t", countQuery = "select count(m.username) from Member m")  // select 쿼리에 join이 많지만 count 쿼리에는 join이 필요 없는 경우 이렇게 설정해놓으면 된다.
+  Page<Member> findByAge(int age, Pageable pageable);
 
 }
